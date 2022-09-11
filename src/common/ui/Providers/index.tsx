30f9e873a97319aae16a3224f57b6c610dd3cc06
Emilio Fernández <emilio.fernandez@cobee.io>
2022-09-11 16:14:24 +0200

    add husky hooks

--- src/common/ui/Providers/index.tsx
+++ src/common/ui/Providers/index.tsx
@@ -1,16 +1,10 @@
 import { ReactNode } from "react";
 import QueryProvider from "./QueryProvider";
-import { GlobalStyles } from "@mui/material";
 
 export interface ProvidersProps {
   children: ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps): JSX.Element {
-  return (
-    <QueryProvider>
-      <GlobalStyles styles={{ body: { margin: 0 } }} />
-      {children}
-    </QueryProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }
